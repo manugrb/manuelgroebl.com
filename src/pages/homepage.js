0195919b1ed0602ea1e5a7b54f344b83932cf915
Manuel Groebl <m.groebl@online.de>
2023-02-19 20:18:14 +0100

    Create new component to style homepage

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Box } from "rebass";
 import BlogpostHomepagePreview from "../components/blogpostHomepagePreview";
 import Layout from "../components/layout";
 import Project from "../components/project";
@@ -30,49 +31,54 @@ const ProjectsDiv = styled.div`
 
 `
 
+const HomePageContentContainer = styled(Box)`
+    background: #f7f8f9;
+`
+
 
 const HomePage = () => {
 
     return(
-        <Layout>
-            <div>
+        
+            <Layout>
                 <div>
-                    <BaseHeader>Hi, I'm Manuel! 👋</BaseHeader>
+                    <div>
+                        <BaseHeader>Hi, I'm Manuel! 👋</BaseHeader>
+                    </div>
+
+                    <div>
+                        <InfoParagraph>I'm a professional full stack developer from germany. I started coding over 3.5 years ago and have been working on multiple comercial projects in the last 2 years.</InfoParagraph>
+                        <InfoParagraph>I really enjoy learning new things, especially new technologies that can help me improve my work. Outside of programming, I'm really intersted in Math and Physics. I love to combine coding with my other interests.</InfoParagraph>
+                        <InfoParagraph>If I'm not coding, I'm likely travelling the world, skiing or hiking around the alps.</InfoParagraph>
+                    </div>
                 </div>
 
                 <div>
-                    <InfoParagraph>I'm a professional full stack developer from germany. I started coding over 3.5 years ago and have been working on multiple comercial projects in the last 2 years.</InfoParagraph>
-                    <InfoParagraph>I really enjoy learning new things, especially new technologies that can help me improve my work. Outside of programming, I'm really intersted in Math and Physics. I love to combine coding with my other interests.</InfoParagraph>
-                    <InfoParagraph>If I'm not coding, I'm likely travelling the world, skiing or hiking around the alps.</InfoParagraph>
-                </div>
-            </div>
+                    <div>
+                        <BaseHeader>Some Notable Projects</BaseHeader> 
+                    </div>
 
-            <div>
-                <div>
-                    <BaseHeader>Some Notable Projects</BaseHeader> 
-                </div>
+                    <ProjectsDiv>
+                        <Project title={"Solar Display"} description={"A web app, that displays everything you need to know about your solar system"} href={"https://github.com/manugrb/alternative-solarmax-display"}/>
+                        <Project title={"Raspberry Pi home server"} description={"A web app for my Raspberry Pi at home to make my life easier"} href={"/whatever"}/>
+                        <Project title={"DIY Bluetooth lights"} description={"A 100% DIY bluetooth controlled dimmable smart LED light for my room"} href={"/bluetoothLights"}/>
+                    </ProjectsDiv>
 
-                <ProjectsDiv>
-                    <Project title={"Solar Display"} description={"A web app, that displays everything you need to know about your solar system"} href={"https://github.com/manugrb/alternative-solarmax-display"}/>
-                    <Project title={"Raspberry Pi home server"} description={"A web app for my Raspberry Pi at home to make my life easier"} href={"/whatever"}/>
-                    <Project title={"DIY Bluetooth lights"} description={"A 100% DIY bluetooth controlled dimmable smart LED light for my room"} href={"/bluetoothLights"}/>
-                </ProjectsDiv>
-
-            </div>
-
-            <div>
-
-                <div>
-                    <BaseHeader>Recent Blog Posts</BaseHeader>
                 </div>
 
                 <div>
-                    <BlogpostHomepagePreview title={"My cool blogpost"} date={"Febuary 15, 2023"} views={512} href={'/test'}/>
+
+                    <div>
+                        <BaseHeader>Recent Blog Posts</BaseHeader>
+                    </div>
+
+                    <div>
+                        <BlogpostHomepagePreview title={"My cool blogpost"} date={"Febuary 15, 2023"} views={512} href={'/test'}/>
+                    </div>
+
                 </div>
 
-            </div>
-            
-        </Layout>
+            </Layout>
 
     )
 
