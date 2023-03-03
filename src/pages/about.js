dcb589e8184e817170dc962b429302a113f7c8ee
Manuel Groebl <m.groebl@online.de>
2023-03-03 17:50:22 +0100

    Create about page

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+import { Box } from "rebass";
+import BlogpostHomepagePreview from "../components/blogpostHomepagePreview";
+import Layout from "../components/layout";
+import Project from "../components/project";
+import BlogPostList from "../components/BlogPostList";
+
+const About = () => {
+
+    return (
+
+        <Layout>
+            <h1>My Cool About page</h1>
+        </Layout>
+
+    );
+
+}
+ 
+export default About;
