a3ac3f0027389af41897e2be570db59809fbe6a4
Manuel Groebl <m.groebl@online.de>
2023-03-31 18:02:48 +0200

    Use new Seo component in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import BlogPostList from "../components/BlogPostList";
 import Frame from "../components/Frame";
 
 import { graphql, useStaticQuery } from "gatsby";
+import Seo from "../components/seo";
 
 const BaseHeader = styled.h1`
     color: #222;
@@ -58,58 +59,64 @@ const HomePage = () => {
 
     return(
 
-        <Frame
-            title={"Manuel Gröbl - professional full stack developer"}
-            description={"Professional full stack web developer with over 3.5 years of experience. Specializes in Javascript and Backend."}
-        >
-            <Layout>
-                <div>
-                    <div>
-                        <BaseHeader>Hi, I'm Manuel! 👋</BaseHeader>
-                    </div>
+        <Layout>
 
-                    <div>
-                        <InfoParagraph>I'm a professional full stack developer from germany. I have over 3.5 years of coding experience and have been working on multiple commercial projects in the last 2 years.</InfoParagraph>
-                        <InfoParagraph>I enjoy learning new things, especially new technologies that can help me improve my work. Outside of programming, I'm really interested in Math and Physics. I love to combine coding with my other interests.</InfoParagraph>
-                        <InfoParagraph>If I'm not coding, I'm likely traveling the world, skiing or hiking around the alps.</InfoParagraph>
-                    </div>
+            
+
+            <div>
+                <div>
+                    <BaseHeader>Hi, I'm Manuel! 👋</BaseHeader>
                 </div>
 
                 <div>
-                    <div>
-                        <BaseHeader>Some Notable Projects</BaseHeader> 
-                    </div>
+                    <InfoParagraph>I'm a professional full stack developer from germany. I have over 3.5 years of coding experience and have been working on multiple commercial projects in the last 2 years.</InfoParagraph>
+                    <InfoParagraph>I enjoy learning new things, especially new technologies that can help me improve my work. Outside of programming, I'm really interested in Math and Physics. I love to combine coding with my other interests.</InfoParagraph>
+                    <InfoParagraph>If I'm not coding, I'm likely traveling the world, skiing or hiking around the alps.</InfoParagraph>
+                </div>
+            </div>
 
-                    <ProjectsDiv>
-                        <Project title={"Solar Display"} description={"A web app, that displays everything you need to know about your solar system"} href={"https://github.com/manugrb/alternative-solarmax-display"}/>
-                        <Project title={"Raspberry Pi home server"} description={"A web app for my Raspberry Pi at home to make my life easier"} href={"/raspberyypihomeserver"}/>
-                        <Project title={"DIY Bluetooth lights"} description={"A 100% DIY bluetooth controlled dimmable smart LED light for my room"} href={"/bluetoothLights"}/>
-                    </ProjectsDiv>
+            <div>
+                <div>
+                    <BaseHeader>Some Notable Projects</BaseHeader> 
+                </div>
 
+                <ProjectsDiv>
+                    <Project title={"Solar Display"} description={"A web app, that displays everything you need to know about your solar system"} href={"https://github.com/manugrb/alternative-solarmax-display"}/>
+                    <Project title={"Raspberry Pi home server"} description={"A web app for my Raspberry Pi at home to make my life easier"} href={"/raspberyypihomeserver"}/>
+                    <Project title={"DIY Bluetooth lights"} description={"A 100% DIY bluetooth controlled dimmable smart LED light for my room"} href={"/bluetoothLights"}/>
+                </ProjectsDiv>
+
+            </div>
+
+            <div>
+
+                <div>
+                    <BaseHeader>Recent Blog Posts</BaseHeader>
                 </div>
 
                 <div>
-
-                    <div>
-                        <BaseHeader>Recent Blog Posts</BaseHeader>
-                    </div>
-
-                    <div>
-                        <BlogPostList>
-                            {blogListData.allMdx.nodes.map(({ frontmatter }) => (
-                                <BlogpostHomepagePreview title={frontmatter.title} date={frontmatter.date} href={"/blog" + frontmatter.slug}/>
-                            ))}
-                        </BlogPostList>
-                    </div>
-
+                    <BlogPostList>
+                        {blogListData.allMdx.nodes.map(({ frontmatter }) => (
+                            <BlogpostHomepagePreview title={frontmatter.title} date={frontmatter.date} href={"/blog" + frontmatter.slug}/>
+                        ))}
+                    </BlogPostList>
                 </div>
 
-            </Layout>
-        </Frame>
+            </div>
 
+        </Layout>
 
     )
 
 }
 
 export default HomePage
+
+
+
+export const Head = () => (
+    <Seo 
+        title={"Manuel Gröbl - professional full stack developer"}
+        description={"Professional full stack web developer with over 3.5 years of experience. Specializes in Javascript and Backend."}
+    />
+)
