d6be4c0416335a30630a8f7815d6a313371f35fd
ManuelGroebl <m.groebl@online.de>
2024-03-19 12:48:33 +0100

    Update theme detection script in ssr

--- gatsby-ssr.jsx
+++ gatsby-ssr.jsx
@@ -9,7 +9,24 @@ exports.onRenderBody = ({
         dangerouslySetInnerHTML={{
             __html: `
             (function() {
-              window.__theme = (window.sessionStorage.getItem('theme') ? window.sessionStorage.getItem('theme') === "true" : window.matchMedia('(prefers-color-scheme: dark)').matches) ? "dark" : "light";
+              setTheme((window.sessionStorage.getItem('theme') ? window.sessionStorage.getItem('theme') === "true" : window.matchMedia('(prefers-color-scheme: dark)').matches) ? "dark" : "light");
+              function setTheme(theme){
+                window.__theme = theme;
+                console.log(document.documentElement);
+                if(window.__theme === "dark"){
+                  document.documentElement.className = 'dark';
+                }else{
+                  document.documentElement.className = '';
+                }
+              }
+              window.__setPreferredTheme = function(theme) {
+                setTheme(theme);
+                try {
+                  window.sessionStorage.setItem('preferred-theme', theme);
+                } catch (e) {}
+              };
+              console.log("TEST TEST TEST TEST TEST");
+              console.log(window.__setPreferredTheme);
               console.log(window.__theme);
               console.log(window.sessionStorage.getItem('theme') + "window.sessionStorage.getItem('theme')");
               console.log(window.matchMedia('(prefers-color-scheme: dark)').matches + "window.matchMedia('(prefers-color-scheme: dark)').matches");
