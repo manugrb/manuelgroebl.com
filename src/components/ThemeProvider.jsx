e940f178027c98fc7b620a3b6b235cfe61410925
ManuelGroebl <m.groebl@online.de>
2024-03-19 12:54:50 +0100

    Handle toggling dark theme with new window.__setPreferredTheme function

--- src/components/ThemeProvider.jsx
+++ src/components/ThemeProvider.jsx
@@ -4,25 +4,6 @@ import styled from "styled-components";
 import DarkModeToggle from "./DarkModeToggle";
 import "../css/globalStyles.css";
 
-const lightTheme = {
-    body: '#f6f7f8',
-    textHighlight: '#222222',
-    textNormal: '#444444',
-    textUnimportant: '#888888',
-    separatorColor: 'rgba(0,0,0,0.1)',
-    shadowColor: '#dddddd',
-    footerColor: '#ffffff'
-}
-const darkTheme = {
-    body: '#121212',
-    textHighlight: '#ffffff',
-    textNormal: '#e6e6e6',
-    textUnimportant: '#a7a7a7',
-    separatorColor: 'rgba(255,255,255,0.1)',
-    shadowColor: '#060606',
-    footerColor: '#191919'
-}
-
 const ToggleContainer = styled.div`
     padding-top: 6px;
 `
@@ -36,14 +17,12 @@ const ThemeProvider = () => {
     const [isDarkTheme, setIsDarkTheme] = useState(prefersDark);
 
     const setThemeFunction = (darkTheme) => {
-
-                if(isBrowser){
+        window.__setPreferredTheme(darkTheme? 'dark' : 'light');
+        if(isBrowser){
             window.sessionStorage.setItem('theme', darkTheme);
             setIsDarkTheme(darkTheme);
         }
-
     }
-
 
     return(
         <ToggleContainer>
